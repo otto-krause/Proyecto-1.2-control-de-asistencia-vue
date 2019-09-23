3c37ca8737cae7720c394e21cc5a1381c1a7b9f7
Franco <franco.emanuel.mercado@outlook.com>
2019-09-23 08:11:39 -0300

    aaaa

--- src/components/VerAsistencia.vue
+++ src/components/VerAsistencia.vue
@@ -1,5 +1,39 @@
 <template>
-    <h1> Lista </h1>
+    <b-table-simple>
+        <b-thead>
+            <tr>
+                <th>IdAsistencia</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Fecha</th>
+            </tr>
+        </b-thead>
+        <b-tbody>
+            <tr v-for="Asistencia in Asistencia" v-bind:key="Asistencia">
+                <td>{{Alumno.IdAsistencia}}</td>
+                <td>{{Alumno.Nombre}}</td>
+                <td>{{Alumno.Apellido}}</td>
+                <td>{{Alumno.Fecha}}</td>
+            </tr>
+        </b-tbody>
+    </b-table-simple>
 </template>
 <script>
+
+export default {
+    components:
+    {
+
+    },
+    data()
+    {
+        return{
+            Asistencia : {}
+        }
+    },
+    methods:
+    {
+
+    }
+}
 </script>
--- src/components/tomarLista.vue
+++ src/components/tomarLista.vue
@@ -2,34 +2,35 @@
     <form action= '/Agregar' method = "POST">    
         <h1>Seleccione un curso</h1>
         <b-dropdown right text="División">
-            <b-dd-item> Curso1</b-dd-item>
-            <b-dd-item> Curso2 </b-dd-item>
-            <b-dd-item> Curso3</b-dd-item>
+            <b-dd-item v-for="Curso in Cursos" v-bind:key = "Curso"><b-button block variant="dark" @click ="ObtenerChabones(Curso.idDivision)"> {{Curso.Division}}</b-button></b-dd-item>
         </b-dropdown>
+        <b-input type="date"></b-input>
         <div class="TablaTomarLista">
-            <b-table-simple>
+            <b-table-simple responsive>
                 <b-thead>
                     <tr>
-                        <th>ID</th>
+                        <th>DNI</th>
                         <th>Nombre</th>
                         <th>Apellido</th>
-                        <th>Presente</th>
-                        <th>Tarde</th>
-                        <th>Ausente con Permanencia</th>
-                        <th>Ausente</th>
-                        <th>Justificado</th>
+                        <th>Asistencia</th>
                     </tr>
                 </b-thead>
                 <b-tbody>
                     <tr v-for="Alumno in Alumnos" v-bind:key="Alumno">
-                        <td name = "idAlumno"></td> <!--Esto va a ser reemplazado por un h1 o algo de nombre "idAlumno" que sea reemplazado por la parte Front End con las cosas de la bd-->
-                        <td>{{alumno.Nombre}}</td>
-                        <td>{{alumno.Apellido}}</td>
-                        <td><input type="radio" name = "valor" value = "P"></td>
-                        <td><input type="radio" name = "valor" value = "T"></td>
-                        <td><input type="radio" name = "valor" value = "AP"></td>
-                        <td><input type="radio" name = "valor" value = "A"></td>
-                        <td><input type="radio" name = "valor" value = "J"></td>
+                        <td>{{Alumno.DNI}}</td>
+                        <td>{{Alumno.Nombre}}</td>
+                        <td>{{Alumno.Apellido}}</td>
+                        <td>
+                            <b-form-group>
+                                <b-form-radio-group      
+                                    id="btn-radios-1"
+                                    v-model="selected"
+                                    :options="Opciones"
+                                    buttons
+                                    name="radios-btn-default"
+                                ></b-form-radio-group>
+                            </b-form-group>
+                        </td>
                     </tr>
                 </b-tbody>
             </b-table-simple>
@@ -45,16 +46,43 @@ export default {
     data()
     {
         return{
-            Alumnos :{}
+            Alumnos :{},
+            Opciones: [
+                {text: 'Presente'},
+                {text: 'Tarde'},
+                {text: 'Ausente con Permanencia'},
+                {text: 'Ausente'},
+                {text: 'Ausente Justificado'}
+            ],
+            Cursos:{}
         }
     },
     mounted()
-    {
-        axios.get('http://localhost:3000/ListaAlumnos').then(
+    {  
+        this.ObtenerChabones(1);
+        this.ObtenerCursos();
+    },
+    methods:{
+        ObtenerChabones(division)
+        {            
+            axios.get('http://localhost:3000/ListaAlumnos/' + division).then(
             (response)=>{
                 this.Alumnos = response.data;
-            }
-        )
+            });
+        },
+        AgregarChabones()
+        {
+            axios.post('http://localhost:3000/Agregar/').then(
+
+            )
+        },
+        ObtenerCursos()
+        {
+            axios.get('http://localhost:3000/ObtenerCursos/').then(
+            (response)=>{
+                this.Cursos = response.data;
+            });
+        }
     }
 }
 </script>
