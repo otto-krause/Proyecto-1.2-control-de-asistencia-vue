f3f3720d5b9b5a9eee1ff04a08000d2c2ab1cbe7
Franco <franco.emanuel.mercado@outlook.com>
2019-10-14 21:43:05 -0300

    Ni voy a hacer como si supiera qué agregué y qué no

--- src/components/TomarLista.vue
+++ src/components/TomarLista.vue
@@ -1,70 +1,81 @@
 <template>
   <div>
     <b-form @submit.prevent="PostAsistencia">
-      <h1>Seleccione un curso</h1>
+      <h1 v-if="!division">Seleccione un curso</h1>
+      <h1 v-if="division && !fecha">Seleccione una fecha</h1>
+      <h1 v-if="division && fecha && !turno">Seleccione un turno</h1>
+      <h1 v-if="division && fecha && turno">Tome asistencia </h1>
       <b-alert v-show="exito" show variant="success">¡Se tomó lista con éxito!</b-alert>
-      <b-dropdown right text="División">
-        <b-dd-item v-for="(Curso, index) in Cursos" v-bind:key="index">
-          <b-button block variant="dark" @click="ObtenerAlumnos(Curso.idDivision)">{{Curso.Division}}</b-button>
-        </b-dd-item>
-      </b-dropdown>
-      {{Semana}}
-      <b-dropdown right v-model="turno" text="Turno">
-        <b-dd-item v-for="(Turno, index) in Semana" v-bind:key="index">
-          {{MostrarAlgo(Turno)}}
-          <b-button block variant="dark">{{Turno.turno}}</b-button>
-        </b-dd-item>
-      </b-dropdown>
-      <b-input @change="ObtenerTurnos(Cursos[0].idDivision,fecha)" type="date" v-model="fecha"></b-input>
-      <div class="TablaTomarLista">
-        <b-table-simple responsive>
-          <b-thead>
-            <tr>
-              <th>DNI</th>
-              <th>Nombre</th>
-              <th>Apellido</th>
-              <th>Asistencia</th>
-            </tr>
-          </b-thead>
-          <b-tbody>
-            <tr v-for="(Alumno, index) in Alumnos" v-bind:key="index">
-              <template>
-                <td>{{Asistencia[index].dniAlumno = Alumno.dniAlumno}}</td>
-                <td>{{Alumno.Nombre}}</td>
-                <td>{{Alumno.Apellido}}</td>
-                <td>
-                  <b-form-group>
-                    <b-form-radio-group
-                      id="'radios'+index"
-                      v-model="Asistencia[index].valor"
-                      buttons
-                      :options="Opciones"
-                      name="'radios'+index"
-                    ></b-form-radio-group>
-                  </b-form-group>
-                </td>
-              </template>
-            </tr>
-          </b-tbody>
-        </b-table-simple>
-      </div>
+      <b-row>
+        <b-col>
+          <article class="card-group-item">
+           <multiselect @change="ObtenerAlumnos(division)" class="col-sm-7" v-model="division" :options="divisiones" :disabled= "divisiones ? false : true" :searchable="false" :close-on-select="true" :show-labels="false" placeholder="Divisiones"></multiselect>
+          </article>
+        </b-col>
+
+        <!--<b-col>
+          <b-dropdown right text="División">
+            <b-dd-item v-for="(Curso, index) in Cursos" v-bind:key="index">
+              <b-button block variant="dark" @click="ObtenerAlumnos(Curso.idDivision)">{{Curso.Division}}</b-button>
+            </b-dd-item>
+          </b-dropdown>
+        </b-col>
+        <article class="card-group-item">
+          <multiselect class="col-sm-7" v-model="division" :options="divisiones" :disabled= "divisiones ? false : true" :searchable="false" :close-on-select="true" :show-labels="false" placeholder="Divisiones"></multiselect>
+        </article>
+        
+        ObtenerTurnos(Cursos[0].idDivision,fecha)
+         -->
+
+        <b-col>
+          <b-input type="date" v-model="fecha"></b-input>
+        </b-col>
+
+        <b-col>
+          <b-dropdown right v-model="turno" text="Turno">
+            <b-dd-item v-for="(Turno, index) in Semana" v-bind:key="index">
+              <b-button block variant="dark">{{Turno.turno}}</b-button>
+            </b-dd-item>
+          </b-dropdown>               
+        </b-col>
+
+      </b-row>
+      <br>  
+        <b-table class="table mx-6" :items="AlumnosFiltrados" :fields="[ { key: 'dniAlumno', label: 'DNI' }, 'Nombre', 'Apellido', 'Asistencia']">
+          <template v-slot:cell(Asistencia)="data">
+            <b-form-group>
+              <b-form-radio-group
+                id="'radios'+data.index"
+                v-model="Asistencia[data.index].valor"
+                buttons
+                container="fluid"
+                :options="Opciones"
+                name="'radios'+data.index"
+              ></b-form-radio-group>
+            </b-form-group>
+          </template>
+        </b-table> 
       <b-button type="submit">Enter</b-button>
     </b-form>
-    <b-button @click="MostrarAlgo(Asistencia[1].valor)"></b-button>
   </div>
 </template>
 <script type="text/javascript">
 
 import axios from 'axios';
+import multiselect from 'vue-multiselect'
+
 
 export default {
   components:
   {
+    multiselect
   },
   name: "TomarLista",
   data() {
     return {
       turno:"",
+      division:"",
+      divisiones:[],
       exito: false,
       semanaActual:{},
       Semana:[],
@@ -81,8 +92,18 @@ export default {
       Cursos: {}
     };
   },
+  computed:{
+    AlumnosFiltrados: function() {
+      return this.Alumnos.filter(alumno => {
+        return (
+          alumno.Division.includes(this.division)
+        );
+      });
+    },
+  },
   mounted() {
     this.ObtenerCursos();
+    this.ObtenerAlumnos();
   },
   methods: {
     MostrarAlgo(algo)
@@ -119,14 +140,14 @@ export default {
     },
     ObtenerTurnos(division, diaSemana)
     {
-      axios.get("http://localhost:3000/ObtenerSemSistencia/" + division + "/" + diaSemana)
+      axios.get("http://localhost:3001/ObtenerSemSistencia/" + division + "/" + diaSemana)
       .then(response =>{
           this.Semana = response.data;
       });
     },
-    ObtenerAlumnos(division) {
+    ObtenerAlumnos() {
       console.log("Intentando obtener alumnos");
-      axios.get("http://localhost:3000/ListaAlumnos/" + division)
+      axios.get("http://localhost:3001/ListaAlumnos/")
         .then(response => {
           this.Alumnos = response.data;
           this.GenerarVacias(this.Alumnos.length);
@@ -139,18 +160,19 @@ export default {
       });
     },
     ObtenerCursos() {
-      axios.get("http://localhost:3000/ObtenerCursos/").then(response => {
+      axios.get("http://localhost:3001/ObtenerCursos/").then(response => {
         this.Cursos = response.data;
+        this.divisiones = [];
+        this.Cursos.forEach(curso =>
+        {
+          this.divisiones.push(curso.Division);
+        })
       });
     }
   }
 };
 </script>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"> </style>
 <style>
-th,
-td {
-  background-color: white;
-  border: 1px solid grey;
-  border-radius: 15px;
-}
+
 </style>
--- src/components/VerAsistencia.vue
+++ src/components/VerAsistencia.vue
@@ -1,41 +1,516 @@
 <template>
-    <b-table-simple>
-        <b-thead>
-            <tr>
-                <th>IdAsistencia</th>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Fecha</th>
-            </tr>
-        </b-thead>
-        <b-tbody>
-            <tr v-for="Asistencia in Asistencia" v-bind:key="Asistencia">
-                <td>{{Alumno.IdAsistencia}}</td>
-                <td>{{Alumno.Nombre}}</td>
-                <td>{{Alumno.Apellido}}</td>
-                <td>{{Alumno.Fecha}}</td>
-            </tr>
-        </b-tbody>
-    </b-table-simple>
+  <div>
+      <h1> Ver Asistencias </h1>
+    <b-tabs>
+      <b-tab title="División y Fecha" active>
+        <b-row>
+          <b-col>
+            <b-card>
+              <b-dropdown right text="División" container="fluid" ref="division" class="w-100">
+                <b-dd-item v-for="(Curso, index) in Cursos" v-bind:key="index">
+                  <b-dropdown-item @click="divisionFiltro = Curso.Division">{{Curso.Division}}</b-dropdown-item>
+                </b-dd-item>
+              </b-dropdown>
+              <b-input type="date" v-model="fecha"></b-input>
+            </b-card>
+          </b-col>
+          <b-col cols="9">
+            <b-pagination
+              v-model="paginaActual"
+              :total-rows="largoFDF"
+              :per-page="porPagina"
+              aria-controls="tablaAsistAlum"
+              align="fill"
+              size="sm"
+            ></b-pagination>
+
+            <b-table
+              id="tablaAsistAlum"
+              :items="FiltradoPorDyF"
+              :per-page="porPagina"
+              :current-page="paginaActual"
+              small
+            ></b-table>
+          </b-col>
+        </b-row>
+      </b-tab>
+      <b-tab title="Alumno">
+        <transition name="fade">
+          <b-row v-if="mostrarCardsAlum">
+            <b-col>
+              <b-card>
+                <b-input type="text" placeholder="NombreAlumno" v-model="nombreFiltro"></b-input>
+                <b-input type="text" placeholder="dniAlumno" v-model="dniFiltro"></b-input>
+                <b-dropdown
+                  right
+                  text="División"
+                  v-model="divisionFiltro"
+                  container="fluid"
+                  class="w-100"
+                >
+                  <b-dd-item v-for="(Curso, index) in Cursos" v-bind:key="index">
+                    <b-button
+                      block
+                      variant="dark"
+                      @click="divisionFiltro = Curso.Division"
+                    >{{Curso.Division}}</b-button>
+                  </b-dd-item>
+                </b-dropdown>
+                <h5>{{divisionFiltro}}</h5>
+              </b-card>
+            </b-col>
+            <b-col cols="9">
+              <div>
+                <b-card-group deck>
+                  <div v-for="(Alumno,index) in FiltradoPorA" v-bind:key="index">
+                    <b-card style="max-width: 15rem; min-width: 15rem">
+                      <b-card-text>{{Alumno.Nombre}}</b-card-text>
+                      <b-card-text>{{Alumno.Apellido}}</b-card-text>
+                      <b-card-text>{{Alumno.Division}}</b-card-text>
+                      <b-card-text>Faltas totales: jaja algún día</b-card-text>
+                      <b-button @click="MostrarDetalle(Alumno)">Ver más</b-button>
+                    </b-card>
+                  </div>
+                </b-card-group>
+              </div>
+            </b-col>
+          </b-row>
+          <div v-else>
+            <b-button @click="Volver()" class="float-left">Volver</b-button>
+            <h3>{{alumnoActual.Nombre + " " + alumnoActual.Apellido}}</h3>
+            <h5>{{alumnoActual.DNI}}</h5>
+            <h5>{{alumnoActual.Division}}</h5>
+            <b-pagination
+              class="bg-variant-danger"
+              v-model="paginaActualB"
+              :total-rows="largoFA"
+              :per-page="porPaginaB"
+              aria-controls="tablaAsistAlum"
+              align="fill"
+              size="sm"
+            ></b-pagination>
+
+            <b-table
+              id="tablaAsistAlum"
+              :items="AsistenciaAlumno(alumnoActual.DNI)"
+              :per-page="porPaginaB"
+              :current-page="paginaActualB"
+              small
+            ></b-table>
+          </div>
+        </transition>
+      </b-tab>
+    </b-tabs>
+  </div>
 </template>
 
 <script>
-
 export default {
-    components:
-    {
-
+  components: {
+  },
+  mounted() {
+    this.ObtenerCursos();
+  },
+  computed: {
+    FiltradoPorDyF: function() {
+      return this.Asistencia.filter(asistencia => {
+        return (
+          asistencia.division.includes(this.divisionFiltro) &&
+          asistencia.fecha.toString().includes(this.fecha.toString())
+        );
+      });
     },
-    data()
-    {
-        return{
-            Asistencia : {}
-        }
+    FiltradoPorA: function() {
+      return this.Alumnos.filter(alumno => {
+        return (
+          (alumno.Nombre.toLowerCase().includes(
+            this.nombreFiltro.toLowerCase()
+          ) ||
+            alumno.Apellido.toLowerCase().includes(
+              this.nombreFiltro.toLowerCase()
+            )) &&
+          alumno.DNI.includes(this.dniFiltro) &&
+          alumno.Division.toString().includes(this.divisionFiltro.toString())
+        );
+      });
     },
-    methods:
-    {
-
+    largoFDF: function() {
+      return this.FiltradoPorDyF.length;
+    },
+    largoFA: function() {
+      return this.FiltradoPorA.length;
     }
-}
-
+  },
+  methods: {
+    ObtenerCursos() {
+      const datos = [
+        {
+          usuario: "User1",
+          idDivision: "1",
+          Division: "6CP1"
+        },
+        {
+          usuario: "User1",
+          idDivision: "2",
+          Division: "6CP2"
+        }
+      ];
+      this.Cursos = datos;
+    },
+    AsistenciaAlumno(dniAlum) {
+      return this.Asistencia.filter(asistencia => {
+        return asistencia.dniAlumno === dniAlum;
+      });
+    },
+    Volver() {
+      this.mostrarCardsAlum = !this.mostrarCardsAlum;
+    },
+    MostrarDetalle(Alumno) {
+      this.alumnoActual = Alumno;
+      this.AsistenciaAlumno(Alumno.DNI);
+      this.mostrarCardsAlum = !this.mostrarCardsAlum;
+    }
+  },
+  data() {
+    return {
+      paginaActual: 1,
+      paginaActualB: 1,
+      porPaginaB: 20,
+      porPagina: 11,
+      divisionFiltro: "",
+      alumnoActual: "",
+      dniFiltro: "",
+      nombreFiltro: "",
+      mostrarCardsAlum: true,
+      fecha: "",
+      Cursos: {},
+      Alumnos: [
+        {
+          DNI: "434343433",
+          Nombre: "Nombre1",
+          Apellido: "Apellido1",
+          Division: "6CP1"
+        },
+        {
+          DNI: "434343434",
+          Nombre: "Nombre2",
+          Apellido: "Apellido2",
+          Division: "6CP1"
+        },
+        {
+          DNI: "434343435",
+          Nombre: "Nombre5",
+          Apellido: "Apellido5",
+          Division: "6CP1"
+        },
+        {
+          DNI: "434343436",
+          Nombre: "Nombre3",
+          Apellido: "Apellido3",
+          Division: "6CP1"
+        },
+        {
+          DNI: "434343437",
+          Nombre: "Nombre4",
+          Apellido: "Apellido4",
+          Division: "6CP1"
+        },
+        {
+          DNI: "13232",
+          Nombre: "no",
+          Apellido: "Apellidow",
+          Division: "6CP2"
+        },
+        {
+          DNI: "798462146/84",
+          Nombre: "Nombre7",
+          Apellido: "Apellido7",
+          Division: "6CP2"
+        },
+        {
+          DNI: "6546546556",
+          Nombre: "Alumno",
+          Apellido: "ApellidoAlumno",
+          Division: "6CP2"
+        }
+      ],
+      Asistencia: [
+        {
+          valor: "T",
+          nombre: "NombreAlumno1",
+          apellido: "ApellidoAlumno1",
+          dniAlumno: "434343433",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "T",
+          nombre: "NombreAlumno1",
+          apellido: "ApellidoAlumno1",
+          dniAlumno: "434343433",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "T",
+          nombre: "NombreAlumno1",
+          apellido: "ApellidoAlumno1",
+          dniAlumno: "434343433",
+          fecha: "2019-10-15",
+          division: "6CP1"
+        },
+        {
+          valor: "T",
+          nombre: "NombreAlumno1",
+          apellido: "ApellidoAlumno1",
+          dniAlumno: "434343433",
+          fecha: "2019-10-15",
+          division: "6CP1"
+        },
+        {
+          valor: "T",
+          nombre: "NombreAlumno1",
+          apellido: "ApellidoAlumno1",
+          dniAlumno: "434343433",
+          fecha: "2019-10-13",
+          division: "6CP1"
+        },
+        {
+          valor: "T",
+          nombre: "NombreAlumno1",
+          apellido: "ApellidoAlumno1",
+          dniAlumno: "434343433",
+          fecha: "2019-10-14",
+          division: "6CP1"
+        },
+        {
+          valor: "T",
+          nombre: "NombreAlumno1",
+          apellido: "ApellidoAlumno1",
+          dniAlumno: "434343433",
+          fecha: "2019-10-16",
+          division: "6CP1"
+        },
+        {
+          valor: "A",
+          nombre: "NombreAlumno2",
+          apellido: "ApellidoAlumno2",
+          dniAlumno: "434343434",
+          fecha: "2019-10-16",
+          division: "6CP1"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno3",
+          apellido: "ApellidoAlumno3",
+          dniAlumno: "434343435",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno4",
+          apellido: "ApellidoAlumno4",
+          dniAlumno: "434343436",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno5",
+          apellido: "ApellidoAlumno5",
+          dniAlumno: "434343437",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno1",
+          apellido: "ApellidoAlumno1",
+          dniAlumno: "434343433",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "A",
+          nombre: "NombreAlumno7",
+          apellido: "ApellidoAlumno7",
+          dniAlumno: "434343434",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno3",
+          apellido: "ApellidoAlumno3",
+          dniAlumno: "434343435",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno9",
+          apellido: "ApellidoAlumno9",
+          dniAlumno: "434343436",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno11",
+          apellido: "ApellidoAlumno11",
+          dniAlumno: "434343437",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "A",
+          nombre: "NombreAlumno1",
+          apellido: "ApellidoAlumno1",
+          dniAlumno: "434343433",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "A",
+          nombre: "NombreAlumno13",
+          apellido: "ApellidoAlumno13",
+          dniAlumno: "434343434",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno3",
+          apellido: "ApellidoAlumno3",
+          dniAlumno: "434343435",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno15",
+          apellido: "ApellidoAlumno15",
+          dniAlumno: "434343436",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno16",
+          apellido: "ApellidoAlumno16",
+          dniAlumno: "434343437",
+          fecha: "2019-10-11",
+          division: "6CP1"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno17",
+          apellido: "ApellidoAlumno17",
+          dniAlumno: "13232",
+          fecha: "2019-10-11",
+          division: "6CP2"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno18",
+          apellido: "ApellidoAlumno18",
+          dniAlumno: "798462146/84",
+          fecha: "2019-10-11",
+          division: "6CP2"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno19",
+          apellido: "ApellidoAlumno19",
+          dniAlumno: "6546546556",
+          fecha: "2019-10-11",
+          division: "6CP2"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno21",
+          apellido: "ApellidoAlumno21",
+          dniAlumno: "13232",
+          fecha: "2019-10-11",
+          division: "6CP2"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno22",
+          apellido: "ApellidoAlumno22",
+          dniAlumno: "798462146/84",
+          fecha: "2019-10-11",
+          division: "6CP2"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno23",
+          apellido: "ApellidoAlumno23",
+          dniAlumno: "6546546556",
+          fecha: "2019-10-11",
+          division: "6CP2"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno24",
+          apellido: "ApellidoAlumno24",
+          dniAlumno: "13232",
+          fecha: "2019-10-11",
+          division: "6CP2"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno25",
+          apellido: "ApellidoAlumno25",
+          dniAlumno: "798462146/84",
+          fecha: "2019-10-11",
+          division: "6CP2"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno26",
+          apellido: "ApellidoAlumno26",
+          dniAlumno: "6546546556",
+          fecha: "2019-10-11",
+          division: "6CP2"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno27",
+          apellido: "ApellidoAlumno27",
+          dniAlumno: "13232",
+          fecha: "2019-10-11",
+          division: "6CP2"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno",
+          apellido: "ApellidoAlumno",
+          dniAlumno: "798462146/84",
+          fecha: "2019-10-11",
+          division: "6CP2"
+        },
+        {
+          valor: "P",
+          nombre: "NombreAlumno",
+          apellido: "ApellidoAlumno",
+          dniAlumno: "6546546556",
+          fecha: "2019-10-11",
+          division: "6CP2"
+        }
+      ]
+    };
+  }
+};
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
